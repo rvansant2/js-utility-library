dd926dcf24b6aec7785b8a0b838cf5a907507769
Robert Van Sant <rvansant@Roberts-MacBook-Pro.local>
2013-01-22 23:52:14 -0500

    formatting and more descriptive function names

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 var utils = (function() {
-	
-	var getURLParamsByName = function(keyname) {
+	//return the value of a query string parameter by query string parameter name
+	var getQSParamsByName = function(keyname) {
 		var keyname = keyname.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]"),
 		regexS = "[\\?&]" + keyname + "=([^&#]*)",
 		regex = new RegExp(regexS),
@@ -12,22 +12,23 @@ var utils = (function() {
 		}
 	};
 
-	var urlParamsToObj = function() {
+	//return the current query string parameters as an object, which could be assigned to a local/global variable 
+	var returnQSParamsAsObj = function() {
 		var urlParamsObj = {},
 		match,
-	    pl     = /\+/g,  // Regex for replacing addition symbol with a space
-	    search = /([^&=]+)=?([^&]*)/g,
-	    decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-	    query  = window.location.search.substring(1);
+		pl     = /\+/g,  // Regex for replacing addition symbol with a space
+		search = /([^&=]+)=?([^&]*)/g,
+		decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+		query  = window.location.search.substring(1);
 
-	    while (match = search.exec(query)) {
-	       urlParamsObj[decode(match[1])] = decode(match[2]);
-	    }
-	    return urlParamsObj;
+		while (match = search.exec(query)) {
+   			urlParamsObj[decode(match[1])] = decode(match[2]);
+		}
+		return urlParamsObj;
 	};
 
 	return {
-		getURLParamsByName: getURLParamsByName,
-		urlParamsToObj: 	urlParamsToObj
-	};
+				getQSParamsByName: 		getQSParamsByName,
+				returnQSParamsAsObj: 	returnQSParamsAsObj
+			};
 })();
